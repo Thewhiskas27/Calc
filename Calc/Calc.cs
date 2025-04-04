537fd456a3e0d079e2f2b4da04efb01e28f09d08
thewhiskas27 <daromir63@gmail.com>
2025-04-04 18:46:34 +0200

    Agregar archivos de proyecto.

---
+++ Calc/Calc.cs
@@ -0,0 +1,13 @@
+﻿namespace Calc
+{
+    public class Calc
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add() { return FirstNumber + SecondNumber; }
+        public int Sub() { return FirstNumber - SecondNumber; }
+        public int Mult() { return FirstNumber * SecondNumber; }
+        public int Div() { return FirstNumber / SecondNumber; }
+    }
+}
